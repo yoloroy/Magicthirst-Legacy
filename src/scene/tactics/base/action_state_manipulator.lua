6410ccac469b4f12f0a44d126173f28dce5b9970
yoloroy <slart2108@gmail.com>
2023-05-29 18:42:50 +0300

    :mute: remove ActionStateManipulator:tryChangeState logs

--- src/scene/tactics/base/action_state_manipulator.lua
+++ src/scene/tactics/base/action_state_manipulator.lua
@@ -23,14 +23,10 @@ end
 --- @param actionName string
 --- @return boolean is the state has been changed
 function ActionStateManipulator:tryChangeState(actionName)
-    print("changing state from: "..self._owner.actionName)
-    print("changing state to: "..actionName)
     local currentState = self._states[self._owner.actionName]
-    print("current state transformations: "..table.concat(currentState.possibleTransformations, ", "))
     if not table.contains(currentState.possibleTransformations, actionName) then return false end
     local nextState = self._states[actionName]
     if not nextState.isPossible(self._owner) then return false end
-    print("changing state is possible")
     self._previousState = self._owner.actionName
     self._owner.actionName = actionName
     return true
